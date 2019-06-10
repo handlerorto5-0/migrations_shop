6904da035b1d1b63ed17743314e7209397a2e248
kv-2 <str_token@mail.ru>
2019-06-10 20:16:50 +0300

    rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_170418) do
+ActiveRecord::Schema.define(version: 2019_06_10_164525) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_06_10_170418) do
     t.date "expiration_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "height"
   end
 
 end
